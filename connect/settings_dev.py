334f6c2588280b86cdcf8e865e559680a2d8e7a3
Shubham Dhama <shubhamdhamaofficial@gmail.com>
2018-12-16 00:40:53 +0530

    Add basic templates and model fields (Unstable commit).
    This commit isn't tested for its atomicity, so this may depend upon next
    commit.
    
    It adds various model fields like:
      * UserProfile (based on AbstractUser)
      * Connection
      * UserPosts
    
    To use phone number field I also added phonenumber_field Django app.
    
    UX-wise:
      * It also improves navigation bar using bootstrap.
      * Also added `Django-crispy-forms` package for improved Signup page.
      * Also added `widget_tweaks` package but didn't use it because of all the
        work being done by crispy forms. Still added bs4_form file which uses
        this package and can be used in the future.
    
    It also added pages like login, forgot password etc.
    
    All Migrations aren't included in this commit.

--- connect/settings_dev.py
+++ connect/settings_dev.py
@@ -1,3 +1,8 @@
 AUTH_PASSWORD_VALIDATORS = []
 
 TIME_ZONE = 'Asia/Kolkata'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ social/views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseRedirect
+)
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+from django.views import generic
+from django.views.generic.edit import CreateView
+
+from social.forms import SignUpForm
+
+
+def signup(request: HttpRequest) -> HttpResponse:
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(
+        request,
+        'index.html',
+    )
