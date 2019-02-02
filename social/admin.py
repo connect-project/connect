59341b612c185d8cd4703c253d2017ed34360b24
Shubham Dhama <shubhamdhamaofficial@gmail.com>
2019-02-03 02:07:08 +0530

    admin interface: Use ModelAdmin and @adminl.register.

--- social/admin.py
+++ social/admin.py
@@ -8,8 +8,37 @@ from social.models import (
     UserPost,
 )
 
-admin.site.register(UserProfile)
-admin.site.register(Comment)
-admin.site.register(Connection)
-admin.site.register(Like)
-admin.site.register(UserPost)
+class UserPostInline(admin.TabularInline):
+    model = UserPost
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    filedsets = (
+        ('Profile', {
+            'fields': ('username', ('first_name', 'last_name'), 'email')
+        })
+    )
+    inlines = [UserPostInline]
+    list_display = ('username', 'first_name', 'last_name', 'email', 'phone_number', 'website')
+    list_filter = ('is_active', 'is_staff')
+
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Connection)
+class ConnectionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserPost)
+class UserPostAdmin(admin.ModelAdmin):
+    pass
