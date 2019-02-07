5792a6d4833c2420f15b6a6d89b91ae59b077797
Shubham Dhama <shubhamdhamaofficial@gmail.com>
2019-02-08 02:51:25 +0530

    Add mod_wsgi django app for running server without touching apache configs.

--- connect/settings.py
+++ connect/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'crispy_forms',
+    'mod_wsgi.server',
     'phonenumber_field',
     'social.apps.SocialConfig',
     'widget_tweaks',
