ae7d59ce5d5babb70567bc1d22194211a99a2ea2
Shubham Dhama <shubhamdhamaofficial@gmail.com>
2019-02-17 15:33:59 +0530

    Add basic media uploading ability.

--- connect/settings.py
+++ connect/settings.py
@@ -117,6 +117,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+
 LOGIN_REDIRECT_URL = '/'
 
 if not PRODUCTION:
